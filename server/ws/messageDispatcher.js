d958fd6401beda60afeeb1fb3a584894727801c9
Clayton Grey <clayton.t.grey@gmail.com>
2025-03-05 18:41:15 -0800

    Map refactor

--- server/ws/messageDispatcher.js
+++ server/ws/messageDispatcher.js
@@ -1,103 +1,44 @@
 // ./server/ws/messageDispatcher.js
 import { MESSAGE_TYPES } from "../../shared/wsMessageTypes.js";
-import {
-  handleJoinSession,
-  handleUpgradeUserId,
-  handleDowngradeUserId,
-} from "./handlers/sessionHandlers.js";
-import { handleCursorUpdate } from "./handlers/cursorHandlers.js";
-import {
-  handleElementGrab,
-  handleElementMove,
-  handleElementRelease,
-  handleElementDeselect,
-  handleElementCreate,
-  handleElementDelete,
-  handleElementResize,
-  handleElementResizeEnd,
-} from "./handlers/elementHandlers.js";
-import {
-  handleMakeEditor,
-  handleRemoveEditor,
-  handleKickUser,
-} from "./handlers/permissionHandlers.js";
-import { handleProjectNameChange } from "./handlers/projectHandlers.js";
-import { handleUndo, handleRedo } from "./handlers/undoRedoHandlers.js";
-import { handleChatMessage } from "./handlers/chatHandlers.js";
+import * as sessionHandlers from "./handlers/sessionHandlers.js";
+import * as cursorHandlers from "./handlers/cursorHandlers.js";
+import * as elementHandlers from "./handlers/elementHandlers.js";
+import * as permissionHandlers from "./handlers/permissionHandlers.js";
+import * as projectHandlers from "./handlers/projectHandlers.js";
+import * as undoRedoHandlers from "./handlers/undoRedoHandlers.js";
+import * as chatHandlers from "./handlers/chatHandlers.js";
 
+// Map message types to their respective handler functions.
+const handlerMap = {
+  [MESSAGE_TYPES.JOIN_SESSION]: sessionHandlers.handleJoinSession,
+  [MESSAGE_TYPES.UPGRADE_USER_ID]: sessionHandlers.handleUpgradeUserId,
+  [MESSAGE_TYPES.DOWNGRADE_USER_ID]: sessionHandlers.handleDowngradeUserId,
+  [MESSAGE_TYPES.CURSOR_UPDATE]: cursorHandlers.handleCursorUpdate,
+  [MESSAGE_TYPES.ELEMENT_GRAB]: elementHandlers.handleElementGrab,
+  [MESSAGE_TYPES.ELEMENT_MOVE]: elementHandlers.handleElementMove,
+  [MESSAGE_TYPES.ELEMENT_RELEASE]: elementHandlers.handleElementRelease,
+  [MESSAGE_TYPES.ELEMENT_DESELECT]: elementHandlers.handleElementDeselect,
+  [MESSAGE_TYPES.ELEMENT_CREATE]: elementHandlers.handleElementCreate,
+  [MESSAGE_TYPES.ELEMENT_DELETE]: elementHandlers.handleElementDelete,
+  [MESSAGE_TYPES.ELEMENT_RESIZE]: elementHandlers.handleElementResize,
+  [MESSAGE_TYPES.ELEMENT_RESIZE_END]: elementHandlers.handleElementResizeEnd,
+  [MESSAGE_TYPES.MAKE_EDITOR]: permissionHandlers.handleMakeEditor,
+  [MESSAGE_TYPES.REMOVE_EDITOR]: permissionHandlers.handleRemoveEditor,
+  [MESSAGE_TYPES.KICK_USER]: permissionHandlers.handleKickUser,
+  [MESSAGE_TYPES.PROJECT_NAME_CHANGE]: projectHandlers.handleProjectNameChange,
+  [MESSAGE_TYPES.UNDO]: undoRedoHandlers.handleUndo,
+  [MESSAGE_TYPES.REDO]: undoRedoHandlers.handleRedo,
+  [MESSAGE_TYPES.CHAT_MESSAGE]: chatHandlers.handleChatMessage,
+};
+
+/**
+ * handleIncomingMessage:
+ *   Given a session, incoming data and the WebSocket, it looks up the appropriate
+ *   handler (if any) and calls it.
+ */
 export function handleIncomingMessage(session, data, ws) {
-  switch (data.type) {
-    // Session join/upgrade/downgrade
-    case MESSAGE_TYPES.JOIN_SESSION:
-      handleJoinSession(session, data, ws);
-      break;
-    case MESSAGE_TYPES.UPGRADE_USER_ID:
-      handleUpgradeUserId(session, data, ws);
-      break;
-    case MESSAGE_TYPES.DOWNGRADE_USER_ID:
-      handleDowngradeUserId(session, data, ws);
-      break;
-
-    // Cursor & element manipulation
-    case MESSAGE_TYPES.CURSOR_UPDATE:
-      handleCursorUpdate(session, data, ws);
-      break;
-
-    case MESSAGE_TYPES.ELEMENT_GRAB:
-      handleElementGrab(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_MOVE:
-      handleElementMove(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_RELEASE:
-      handleElementRelease(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_DESELECT:
-      handleElementDeselect(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_CREATE:
-      handleElementCreate(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_DELETE:
-      handleElementDelete(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_RESIZE:
-      handleElementResize(session, data, ws);
-      break;
-    case MESSAGE_TYPES.ELEMENT_RESIZE_END:
-      handleElementResizeEnd(session, data, ws);
-      break;
-
-    // Permissions
-    case MESSAGE_TYPES.MAKE_EDITOR:
-      handleMakeEditor(session, data, ws);
-      break;
-    case MESSAGE_TYPES.REMOVE_EDITOR:
-      handleRemoveEditor(session, data, ws);
-      break;
-    case MESSAGE_TYPES.KICK_USER:
-      handleKickUser(session, data, ws);
-      break;
-
-    // Project name changes
-    case MESSAGE_TYPES.PROJECT_NAME_CHANGE:
-      handleProjectNameChange(session, data, ws);
-      break;
-
-    // Undo/redo
-    case MESSAGE_TYPES.UNDO:
-      handleUndo(session, data, ws);
-      break;
-    case MESSAGE_TYPES.REDO:
-      handleRedo(session, data, ws);
-      break;
-
-    // Chat messages
-    case MESSAGE_TYPES.CHAT_MESSAGE:
-      handleChatMessage(session, data, ws);
-      break;
-
-    default:
-      break;
+  const handler = handlerMap[data.type];
+  if (handler) {
+    handler(session, data, ws);
   }
 }
